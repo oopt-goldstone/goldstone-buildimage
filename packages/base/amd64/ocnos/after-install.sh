6592d29f74fd749f420ec8f8f7bcd30e78fd8a32
KazuyaAnazawa <m5201120.grd@gmail.com>
2023-01-24 08:41:34 +0900

    ocnos: stop enabling ocnos.service in after-install.sh
    We only enable it in init.sh of the platforms that use OcNOS.
    This commit also a little bit improves init.sh of as7316-26xb.

--- packages/base/amd64/ocnos/after-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-systemctl enable ocnos.service
--- packages/platforms/wistron/x86-64/wtp-01-02-00/goldstone-platform-config/builds/init.sh
+++ packages/platforms/wistron/x86-64/wtp-01-02-00/goldstone-platform-config/builds/init.sh
@@ -6,4 +6,3 @@ systemctl enable gs-south-onlp
 systemctl enable gs-south-tai
 systemctl enable gs-south-system
 systemctl enable gs-south-sonic
-systemctl disable ocnos.service
